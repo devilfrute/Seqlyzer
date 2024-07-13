a388edb77c6d82c458d26543f03b0945afa0750f
VAMSI <seerlamahalakshmi1@gmail.com>
2024-07-14 00:47:43 +0530

    added download buton for tarnslated protein

--- Testcode.py
+++ Testcode.py
@@ -11,8 +11,8 @@ if uploaded_file is not None:
     stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
     seq_record = SeqIO.read(stringio, "fasta")
     
-    seq_id = f"Sequence ID  : {seq_record.id}"
-    seq_length = f"Sequence Length : {len(seq_record.seq)}"
+    seq_id = f"**Sequence ID  :** {seq_record.id}"
+    seq_length = f"**Sequence Length :** {len(seq_record.seq)}"
     
     # Calculate and display nucleotide counts
     a_count = seq_record.seq.count("A")
@@ -21,7 +21,7 @@ if uploaded_file is not None:
     c_count = seq_record.seq.count("C")
     
     nucleotide_counts = (
-        f"Nucleotide Counts as follows :\n"
+        f"**Nucleotide Counts as follows :**\n"
         f"Adenine (A) count: {a_count}\n"
         f"Thymine (T) count: {t_count}\n"
         f"Guanine (G) count: {g_count}\n"
@@ -30,19 +30,19 @@ if uploaded_file is not None:
     
     # Calculate and display total nucleotide count
     total_nucleotides = a_count + t_count + g_count + c_count
-    total_nucleotides_str = f"Total Nucleotide Count:{total_nucleotides}"
+    total_nucleotides_str = f"**Total Nucleotide Count:** {total_nucleotides}"
     
     # Calculate and display GC content
     gc_content = (g_count + c_count) / len(seq_record.seq) * 100
-    gc_content_str = f"GC Content: {gc_content:.2f}%"
+    gc_content_str = f"**GC Content:** {gc_content:.2f}%"
     
     # Identify ORFs (simple example considering start codons only)
     orfs = [str(seq_record.seq[i:i+3]) for i in range(0, len(seq_record.seq)-2, 3) if seq_record.seq[i:i+3] == "ATG"]
-    num_orfs = f"Number of ORFs: {len(orfs)}"
+    num_orfs = f"**Number of ORFs:** {len(orfs)}"
     
-    # Translate DNA sequence to protein sequence
+    # Translate DNA sequence to protein sequence, stopping at the first stop codon
     protein_seq = seq_record.seq.translate(to_stop=True)
-    translated_seq_str = f"Translated Protein Sequence:\n{protein_seq}"
+    translated_seq_str = f"**Translated Protein Sequence:**\n{protein_seq}"
     
     # Display results in Streamlit
     st.write(seq_id)
@@ -67,10 +67,18 @@ if uploaded_file is not None:
         f"{translated_seq_str}\n"
     )
     
-    # Provide a download button for the text output
+    # Provide a download button for the complete analysis
     st.download_button(
-        label="Download Results as TXT",
+        label="Download Complete Analysis as TXT",
         data=output,
         file_name="dna_sequence_analysis.txt",
+        mime="text/plain"
+    )
+    
+    # Provide a download button for the translated protein sequence only
+    st.download_button(
+        label="Download Translated Protein Sequence as TXT",
+        data=str(protein_seq),
+        file_name="translated_protein_sequence.txt",
         mime="text/plain"
     )
