050cd9a7c08c9dc7bc768a3bde428c7a619ac184
VAMSI <seerlamahalakshmi1@gmail.com>
2024-08-15 15:17:23 +0530

    removeed

--- Testcode.py
+++ Testcode.py
@@ -1,5 +1,4 @@
-# This is app is created by Chanin Nantasenamat (Data Professor) https://youtube.com/dataprofessor
-# Credit: This app is inspired by https://huggingface.co/spaces/osanseviero/esmfold
+
 
 import streamlit as st
 from stmol import showmol
